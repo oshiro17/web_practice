7255e23724602ce8d6ffdfe3ccdd7481ae9dcb81
oshiro17 <nonokapiano@icloud.com>
2023-02-09 17:48:55 +0900

    rename move.js to index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+window.onload = function () {
+  document.querySelector('#li_about').addEventListener('click', function () {
+    const aboutSection = document.querySelector('#about');
+    aboutSection.scrollIntoView({});
+  });
+  document.querySelector('#li_works').addEventListener('click', function () {
+    const aboutSection = document.querySelector('#works');
+    aboutSection.scrollIntoView({ behavior: 'smooth' });
+  });
+  document.querySelector('#li_news').addEventListener('click', function () {
+    const aboutSection = document.querySelector('#news');
+    aboutSection.scrollIntoView({ behavior: 'smooth' });
+  });
+  document.querySelector('#li_contact').addEventListener('click', function () {
+    const aboutSection = document.querySelector('#contact');
+    aboutSection.scrollIntoView({ behavior: 'smooth' });
+  });
+  document.querySelector('#contact_button').addEventListener('click', function () {
+    let userName = document.getElementById('userName').value;
+    if (!userName) {
+      var tableElem = document.getElementById("contact");
+      var createElement = document.createElement("h1");
+      createElement.innerHTML = "userName is null";
+      tableElem.appendChild(createElement);
+    }
+  });
+}
